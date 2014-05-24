3766fd13a895509713cba89be6f303fc56b33c73
Pavel Kolesnikov <pavel@gooddata.com>
2014-05-23 23:50:35 -0700

    makeCacheMatrix and cacheSolve functions implemented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#' Create a list wrapping a given matrix; the returned list includes
+#' utility methods for caching an inverse matrix. The list is expected
+#' to be used as an argument to the cacheSolve method.
+#'
+#' @param x a matrix to be wrapped and eventually inverted using the
+#'   cacheSolve function
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+#' Returns inverse matrix of the matrix wrapped by a makeCacheMatrix function
+#'
+#' @param x a list returned by the makeCacheMatrix function. x$get must return
+#'   a matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
 }
